cbe826c9a251c52e5e7139cdbfec8614d33c77d3
GerDav-code <gerardodaviddamianalonso@gmail.com>
2026-05-26 07:49:36 -0600

    se agrego ruta de products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
 
+  {
+    path: 'products',
+    component: Products
+  },
   {
     path: 'home',
     component:Home
